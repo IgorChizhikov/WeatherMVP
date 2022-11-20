f9ad4a70e31127ba83753d4d703f5d020b28244f
Игорь <izografik@gmail.com>
2022-11-20 16:46:48 +0300

    - Start new project

---
+++ WeatherMVP/WeatherMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMVP
+//
+//  Created by Игорь on 20.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
